b611639036414dbb007fd97b32754aafadc33012
PARIKSHUKLA <153470611+PARIKSHUKLA@users.noreply.github.com>
2023-12-10 19:27:42 -0600

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title("My New Healthy Dinner")
 streamlit.header("BreakFast Menu")
 streamlit.text("Banana")
 streamlit.text("bread & butter")
-Streamlit.text ("Hot Masala Chai")
+streamlit.text ("Hot Masala Chai")
                
