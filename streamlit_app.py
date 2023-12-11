96bf14515ee165a4f57b830e70a653823febcba7
PARIKSHUKLA <153470611+PARIKSHUKLA@users.noreply.github.com>
2023-12-10 19:26:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title("My New Healthy Dinner")
+
+streamlit.header("BreakFast Menu")
+streamlit.text("Banana")
+streamlit.text("bread & butter")
+Streamlit.text ("Hot Masala Chai")
+               
