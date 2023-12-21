f3ce8f68983adfacbc58bfe6fef430cd37f54e08
PARIKSHUKLA <153470611+PARIKSHUKLA@users.noreply.github.com>
2023-12-21 11:24:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.text("bread & butter")
 streamlit.text ("Hot Masala Chai")
 streamlit.text ("Sandwitch")
 
-import padnas
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
