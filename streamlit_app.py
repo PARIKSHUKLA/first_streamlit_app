30955633ba0131ac5eefeefd765750a1e2aa20e1
PARIKSHUKLA <153470611+PARIKSHUKLA@users.noreply.github.com>
2023-12-21 11:37:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.text ("Sandwitch")
 
 import pandas
 my_fruit_list  = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.mutliselect("Pick some fruits:" , list(my_fruit_list.index))
-streamlit.dataframe(my_fruit_list)
+streamlit.mutliselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe (my_fruit_list)
 
                
